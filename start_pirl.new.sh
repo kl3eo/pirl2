8cf539200523f3c99a2bbedd8d913df7e55dc4b3
kl3eo <alex@motivation.ru>
2021-07-26 21:11:54 +0300

    cpu limit to 50% or slow sync

--- start_pirl.new.sh
+++ start_pirl.new.sh
@@ -14,8 +14,8 @@ cd /usr/local/lib64 && wget https://room-house.com/libstdc++.so.6.0.28
 
 ln -s libstdc++.so.6.0.28 libstdc++.so.6
 
-#change paths, names and ports accordingly
+#change paths, names and ports accordingly; CPU limit 50% +- accordingly
 #also, may need to remove "-b" flag from cpulimit if you're not on ubuntu
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib64/
-/opt/nvme/pirl_0.8.29_07232021 --base-path /ssd/pirl3/ --port 30330 --ws-port 9941 --rpc-port 9930 --bootnodes /dns/cube.room-house.com/tcp/8480/p2p/12D3KooWP3QEtphfi5iTfyss8jf1WYSgywVNDUrPkQukxgmTFuhp --rpc-cors all --validator --name xTER2 & cpulimit -p $! -l 25 -b
+/opt/nvme/pirl_0.8.29_07232021 --base-path /ssd/pirl3/ --port 30330 --ws-port 9941 --rpc-port 9930 --bootnodes /dns/cube.room-house.com/tcp/8480/p2p/12D3KooWP3QEtphfi5iTfyss8jf1WYSgywVNDUrPkQukxgmTFuhp --rpc-cors all --validator --name xTER2 & cpulimit -p $! -l 50 -b
